7418673e79c17eb7b032e5f14d4e0ed266074a08
vmilybrown <emilypobbles@gmail.com>
2024-09-14 20:31:29 +0100

    Final commit

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.querySelectorAll('.scroll-to-bottom').forEach(button => {
+    button.addEventListener('click', function() {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth' // Smooth scrolling effect
+        });
+    });
+});
 
-    const images = document.querySelectorAll("img");
+document.querySelectorAll('.image-container').forEach(container => {
+    container.addEventListener('touchstart', function() {
+        this.querySelector('.overlay').style.transform = 'translateY(0)';
+    });
 
-    for (const image of images) {
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message;
-            image.width = 100;
-            image.height = 100;
-        })
-        .catch(error => console.error('Error fetching the image:', error)); // Optional: error handling
-    }
-
+    container.addEventListener('touchend', function() {
+        this.querySelector('.overlay').style.transform = 'translateY(100%)';
+    });
 });
